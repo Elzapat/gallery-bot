19328abfbe1bbd4016fd72b636f44c746e6151e0
Morgan Van Amerongen <mistigri.van@gmail.com>
2021-04-24 14:52:10 +0200

    deleted build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-const handle_message_1 = require("./handle_message");
-require("dotenv").config();
-const client = new discord_js_1.Client();
-client.once("ready", () => console.log("Ready!"));
-client.on("message", message => handle_message_1.handleMessage(message));
-client.login(process.env.TOKEN);
